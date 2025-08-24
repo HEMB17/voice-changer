b51c79a30d71daea86d8b3b6cd6114c16867cff1
Hugo Melo <development@ibacrea.com>
2025-08-24 13:51:51 -0500

    Add: lock web access

--- server/restapi/mods/trustedorigin.py
+++ server/restapi/mods/trustedorigin.py
@@ -117,6 +117,9 @@ class TrustedOriginMiddleware:
         """
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        
+        logger.info("VALIDANDO ORIGEN HUSA.IA")
+        print("VALIDANDO ORIGEN HUSA.IA")
         if scope["type"] not in (
             "http",
             "websocket",
